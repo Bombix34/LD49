242ee02906f6b223aa491afd699a613afb28ac52
Hugo <bombix34@gmail.com>
2021-10-04 22:36:55 +0200

    airplane fix

--- Assets/Scripts/AirplaneManager.cs
+++ Assets/Scripts/AirplaneManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro; 
 
 public class AirplaneManager : MonoBehaviour
 {
@@ -9,8 +10,11 @@ public class AirplaneManager : MonoBehaviour
     private Vector2 initPosition;
     public Vector2 randomPosY;
 
+    private TextMeshProUGUI airplaneText;
+
     private void Start()
     {
+        airplaneText = GetComponentInChildren<TextMeshProUGUI>();
         initPosition = airPlane.transform.position;
         StartCoroutine(Behavior());
     }
@@ -21,6 +25,7 @@ public class AirplaneManager : MonoBehaviour
         {
             airPlane.transform.position = new Vector3(initPosition.x,initPosition.y +( initPosition.y * (Random.Range(-0.3f,0.6f))), 0);
             yield return new WaitForSeconds(Random.Range(15f, 30f));
+            airplaneText.text = "Je suis un texte d'exemple";
             airPlane.transform.DOMoveX(-Screen.width/2, 10f);
         }
     }
