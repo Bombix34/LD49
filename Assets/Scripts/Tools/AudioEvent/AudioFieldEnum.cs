f02fe3fd0228c362e1c7dbc4804aff5c3fb47705
Hugo <bombix34@gmail.com>
2021-10-04 21:33:40 +0200

    premier jet integration sonore

---
+++ Assets/Scripts/Tools/AudioEvent/AudioFieldEnum.cs
@@ -0,0 +1,14 @@
+public enum AudioFieldEnum 
+{
+	sfx_house = 0,
+	sfx_appartments = 1,
+	sfx_office = 2,
+	sfx_mall = 3,
+	sfx_factory = 4,
+	sfx_powerplant = 5,
+	sfx_cinema = 6,
+	sfx_casino = 7,
+	sfx_road = 8,
+	sfx_burn = 9,
+
+}
